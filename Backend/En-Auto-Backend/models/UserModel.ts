91a61afd0ff58990214d89a6023a29ce9616ee90
Victor <victor-smi@hotmail.fr>
2020-03-10 11:32:55 +0100

    Mongo user model
    Array of user :

--- Backend/En-Auto-Backend/models/UserModel.ts
+++ Backend/En-Auto-Backend/models/UserModel.ts
@@ -1,34 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 if (mongoose.connection.readyState === 0) {
-    mongoose.connect(require('./connection-string'));
+    mongoose.connect(require(process.env.MONGO));
 }
-
-const user = new Schema({
-
+const user = {
     '_id': {type: Schema.Types.ObjectId, ref: ''},
     'lastName': {type: String},
     'firstName': {type: String},
     'password': {type: String},
-    'address': {type: String},
     'email': {type: String},
+    'id_devis': {type: [Number]},
     'createdAt': {type: Date, default: Date.now},
     'updatedAt': {type: Date, default: Date.now}
+};
+
+const users = new Schema({
+    users : {type: [user]}
 });
 
-user.pre('save', function(next){
+users.pre('save', function (next) {
     this.updatedAt = Date.now();
     next();
 });
 
-user.pre('update', function() {
-    this.update({}, { $set: { updatedAt: Date.now() } });
+users.pre('update', function () {
+    this.update({}, {$set: {updatedAt: Date.now()}});
 });
 
-user.pre('findOneAndUpdate', function() {
-    this.update({}, { $set: { updatedAt: Date.now() } });
+users.pre('findOneAndUpdate', function () {
+    this.update({}, {$set: {updatedAt: Date.now()}});
 });
-
 
 
 module.exports = mongoose.model('User', user);
