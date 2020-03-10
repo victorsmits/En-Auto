ab4d6dd506da7997971bc5af4b16e57c94f5f134
reilbat <reilbat25@gmail.com>
2020-03-10 16:30:32 +0100

    DevisModel&Populate
    Model DB pour devis
    et script de population pour tests

---
+++ Backend/En-Auto-Backend/bin/populate.ts
@@ -0,0 +1,51 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require('custom-env').env(process.env.APP_ENV);
+var mongoose_1 = __importDefault(require("mongoose"));
+var User = require('../En-Auto-Backend/models/UserModel');
+var Devis = require('../En-Auto-Backend/models/DevisModel');
+mongoose_1.default.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+//ici on drop 
+mongoose_1.default.connection.collections['users'].drop();
+mongoose_1.default.connection.collections['devis'].drop();
+
+var User1 = new User({
+    _id: 1,
+    firstName: "Tomtom",
+    lastName: "Tona",
+    id_devis: 1,
+    email: "tomtom@gmail.com",
+    password: "@tom"
+});
+var Devis1 = new Devis({
+    id_devis: 1,
+    cout_structure: 500,
+    cout_acheminement: 400,
+    prix_cuve: 150,
+    conso: 100,
+});
+var User2 = new User({
+    _id: 2,
+    firstName: "Nana",
+    lastName: "Tona",
+    id_devis: 1,
+    email: "nana@gmail.com",
+    password: "@na"
+});
+
+User.create([User1, User2])
+    .then(function (data) {
+    console.log(data, "Populated (User)!");
+}).catch(function (err) {
+    console.log("Not Populated (User)...");
+});
+Metric.create([Devis1])
+    .then(function (data) {
+    console.log(data, "Populated (Devis)!");
+    mongoose_1.default.connection.close();
+}).catch(function (err) {
+    console.log("Not Populated (Devis) ...");
+});
