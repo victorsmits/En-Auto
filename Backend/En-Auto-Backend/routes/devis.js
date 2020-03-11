27915b887d5d447a9fb57cebc3eddb4df8855bbd
Iandraina <iandraina.ravelomanana@edu.ece.fr>
2020-03-11 12:20:11 +0100

    route-devis

---
+++ Backend/En-Auto-Backend/routes/devis.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+const Devis = mongoose.model('Devis');
+
+router.post('/create-devis', function(req, res, next) {
+    let devis = new devisSchema ({
+        cout_structure: '', // calcul :  besoin de req.body.installation et de req.body.tuile et faire ensuite un calcul
+        cout_acheminement: "", //Calcul ?
+        prix_cuve : "", //En fonction du choix de l'utilisateur ?
+        conso : "",
+        prix_eaux:"",
+        recup_eaux:"", //req.body.superficie * hauteur pluviométrie
+        superficie : req.body.superficie,
+        economie : "", //
+        rentabilite:""
+    });
+
+    devis.save()
+        .then(r =>{
+            return res.status(201).json(r);
+        }).catch(error => {
+            return res.status(501).json({message : error})
+        })
+});
+
+router.put('/update-devis', function(req, res, next) {
+    
+})
--- docs/Route.md
+++ docs/Route.md
@@ -4,3 +4,4 @@
 |:-:|:-:|:-:|:-:|:-:|
 | /user/login |POST|`{ email : String, password : String}` | `{ connected : JWT }` | `{ message: String }`|
 | /user/register |POST|`{ lastName: String, FirstName: String, email : String, password : String }`| `{User Model}` | `{ message: String }`|
+|/devis/create-devis |POST| `{area : Float, installation : Boolean,  tuile : Float, }` | `{Devis Model}`| `{ message : String}`|
