feb00740ad89514a99c88b0aa90d18b0c87c29aa
Iandraina <iandraina.ravelomanana@edu.ece.fr>
2020-03-12 14:04:30 +0100

    MAJ requête devis

--- Backend/En-Auto-Backend/routes/devis.js
+++ Backend/En-Auto-Backend/routes/devis.js
@@ -3,30 +3,68 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Devis = mongoose.model('Devis');
 
+//Création de devis (données à récup auprès de l'utilisateur)
 router.post('', function(req, res, next) {
     let devis = new Devis ({
-        id_user: req.params.id_user,
-        cout_structure: '', // calcul :  besoin de req.body.installation et de req.body.tuile et faire ensuite un calcul
-        cout_acheminement: "", //Calcul ?
-        prix_cuve : "", //En fonction du choix de l'utilisateur ?
-        conso : "",
-        prix_eaux:"",
-        recup_eaux:"", //req.body.superficie * hauteur pluviométrie
-        superficie : req.body.superficie,
-        economie : "", //
-        rentabilite:""
+        id_user: req.body.id_user,
+        cout_structure: req.body.cout_structure,
+        cout_acheminement: req.body.cout_acheminement,
+        prix_cuve : req.body.prix_cuve, 
+        conso : req.body.conso,
+        superficie : req.body.superficie
     });
 
     devis.save()
         .then(r =>{
             return res.status(201).json(r);
         }).catch(error => {
-            return res.status(401).json({message : error})
+            return res.status(501).json({message : error})
         })
 });
 
-router.put('', function(req, res, next) {
-    
+//Récupération du devis en fonction de l'id_user
+router.get('', function(req, res, next) {
+    Devis.find({id_user : req.query.id_user.toString()},(err,devis)=>{
+        if(!devis) {
+            return res.status(404).send({
+                message : "Devis not found with id "+ req.query.id_user
+            });
+        }else{
+            return res.status(201).json(devis);
+        } 
+    })
 });
+
+//M.A.J du devis utilisateur (dépendant de l'id_user)
+router.put('', function(req, res, next) {
+    Devis.findByIdAndUpdate({id_user: req.body.id_user.toString()}, {
+        cout_structure: req.body.cout_structure,
+        cout_acheminement: req.body.cout_acheminement,
+        prix_cuve : req.body.prix_cuve,
+        conso : req.body.conso,
+        superficie : req.body.superficie
+    }, (err, devis) => {
+        console.log("devis= ",devis);
+        if(!devis) {
+            return res.status(404).send({
+                message : "Devis not found with id "+ req.body.id_user
+            });
+        }
+        res.send({message: "Devis updated successfully!"});
+    })
+})
+
+//Suppression du devis utilisateur (dépendant de l'id_user)
+router.delete('', function(req, res, next) {
+    Devis.findByIdAndRemove({id_user: req.query.id_user.toString()}, (err,devis) =>{
+        console.log("devis= ",devis)
+        if(!devis) {
+            return res.status(401).send({
+                message : "Devis not found with id "+ req.query.id_user
+            });
+        }
+        res.send({message: "Devis deleted successfully!"});
+    });
+})
 
 module.exports = router;
--- docs/Route.md
+++ docs/Route.md
@@ -4,4 +4,7 @@
 |:-:|:-:|:-:|:-:|:-:|
 | /user/login |POST|`{ email : String, password : String}` | `{ connected : JWT }` | `{ message: String }`|
 | /user/register |POST|`{ lastName: String, FirstName: String, email : String, password : String }`| `{User Model}` | `{ message: String }`|
-|/devis/create-devis |POST| `{area : Float, installation : Boolean,  tuile : Float, }` | `{Devis Model}`| `{ message : String}`|
+| /devis |POST| `{houseArea : Float, installation : Boolean,  tuile : Float, }` | `{Devis Model}`| `{ message : String}`|
+  /devis |GET|` `|`{message : String}` |{message: String}|
+| /devis |PUT| `{houseArea : Float, installation : Boolean,  tuile : Float, }` | `{message: String}`| `{ message : String}`|
+/ devis |DELETE|` `|`{message: String}` |`{message: String}`|
