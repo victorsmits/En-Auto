3509b82330d0c818d1968619dc970b5d4a919750
Victor <victor-smi@hotmail.fr>
2020-04-06 16:35:20 +0200

    update Heroku

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .;
 git commit -m "update Heroku";
-git subtree push --force --prefix En-Auto-Backend https://git.heroku.com/en-auto-backend.git master;
-git subtree push --force --prefix En-Auto-Frontend https://git.heroku.com/en-auto-frontend.git master
+git subtree push --prefix En-Auto-Backend https://git.heroku.com/en-auto-backend.git master;
+git subtree push --prefix En-Auto-Frontend https://git.heroku.com/en-auto-frontend.git master
