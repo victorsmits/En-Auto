02b84687adf99bb67e65f1102502a690a8a17c12
Victor <victor-smi@hotmail.fr>
2020-04-07 11:54:48 +0200

    navigate with enter key

--- En-Auto-Frontend/src/app/estimation/estimation.component.ts
+++ En-Auto-Frontend/src/app/estimation/estimation.component.ts
@@ -44,7 +44,6 @@ export class EstimationComponent implements OnInit, AfterViewInit {
       this.saveDevis();
     }
 
-
     this.estiForm = this.fb.group({
       address: ['', [Validators.required, Validators.pattern('(\\d{1,}) [a-zA-Z0-9\\s]+(\\.)?')]],
       postalCode: [null, [Validators.required, Validators.pattern('[0-9]{5}')]],
@@ -76,50 +75,20 @@ export class EstimationComponent implements OnInit, AfterViewInit {
   computeFirstDevis() {
     this.water_volume = this.math.roofWaterVolume(this.estiForm.value['houseArea'], 10, this.estiForm.value['roof_type']);
 
-  computeFinalDevis() {
-    let water;
-    let consommation = this.estiForm.value['consommation'] ? this.estiForm.value['consommation'] : this.math.calc_conso(this.estiForm);
-    let vol_storage = this.math.calc_vol_storage(this.water_volume, consommation);
     this.api.getWaterCost(this.estiForm.value['postalCode']).subscribe(data => {
       if (data[0]) {
         this.water_cost = this.tool.setWaterCost(data[0]).cost;
       } else {
         this.water_cost = 0;
       }
-      this.finalDevis = {
-        //required
-        _id: this.estiForm.value['_id'] ? this.estiForm.value['_id'] : undefined,
-        address: this.estiForm.value['address'] ? this.estiForm.value['address'] : undefined,
-        code_postal: this.estiForm.value['postalCode'] ? this.estiForm.value['postalCode'] : undefined,
-        id_user: this.tool.isLoggedIn() ? this.tool.getUser()._id : undefined,
-        routing_cost: 100,
-
-        water_cost: water, //cout de l'eau recuperer sur BDD
-
-        // non required
-        tiles_cost: this.tiles_cost,
-        tiles_number: this.estiForm.value['tiles_nb'] ? this.estiForm.value['tiles_nb'] : undefined,
-        structural_cost: this.roof_cost,
-        tank_type: this.estiForm.value['tank_type'],
-        tank_dist: this.estiForm.value['tank_dist'],
-        consum: consommation,
-        water_volume: this.water_volume, //volume d'eau recupéré
-        roof_area: this.estiForm.value['houseArea'] ? this.estiForm.value['houseArea'] : undefined,
-        vol_storage: vol_storage,
-        tank_cost: this.math.calc_cost_tank(this.estiForm, vol_storage),
-        final_save: this.estiForm.value['final_save'] ? this.estiForm.value['final_save'] : undefined,
-        rentability: undefined,
-        created_at: this.estiForm.value['created_at'] ? this.estiForm.value['created_at'] : new Date(),
-      };
     });
   }
 
   computeFinalDevis() {
-    let gutter = this.estiForm.value['m_gutter'];
-    let tiles = this.estiForm.value['tiles_nb'];
+    let water;
+    let consommation = this.estiForm.value['consommation'] ? this.estiForm.value['consommation'] : this.math.calc_conso(this.estiForm);
+    let vol_storage = this.math.calc_vol_storage(this.water_volume, consommation);
 
-    this.tiles_cost = tiles != null ? this.math.tilesReparationCost(tiles) : 0;
-    this.gutter_cost = gutter != null ? this.math.gutterReparationCost(gutter) : 0;
 
     this.finalDevis = {
       //required
@@ -127,18 +96,21 @@ export class EstimationComponent implements OnInit, AfterViewInit {
       address: this.estiForm.value['address'] ? this.estiForm.value['address'] : undefined,
       code_postal: this.estiForm.value['postalCode'] ? this.estiForm.value['postalCode'] : undefined,
       id_user: this.tool.isLoggedIn() ? this.tool.getUser()._id : undefined,
-      routing_cost: 10,
-      tank_cost: 10,
-      water_cost: this.water_cost, //cout de l'eau recuperer sur BDD
+      routing_cost: 100,
+
+      water_cost: water, //cout de l'eau recuperer sur BDD
 
       // non required
       tiles_cost: this.tiles_cost,
       tiles_number: this.estiForm.value['tiles_nb'] ? this.estiForm.value['tiles_nb'] : undefined,
-      structural_cost: this.tiles_cost + this.gutter_cost,
-      consum: this.estiForm.value['consommation'] ? this.estiForm.value['consommation'] : undefined,
+      structural_cost: this.roof_cost,
+      tank_type: this.estiForm.value['tank_type'],
+      tank_dist: this.estiForm.value['tank_dist'],
+      consum: consommation,
       water_volume: this.water_volume, //volume d'eau recupéré
-      roof_area: this.estiForm.value['houseArea'] ? this.estiForm.value['houseArea'] : this.math.calc_vol_storage(this.finalDevis),
-      vol_storage: this.estiForm.value["vol_storage"] ? this.math.calc_vol_storage(this.finalDevis) : undefined,
+      roof_area: this.estiForm.value['houseArea'] ? this.estiForm.value['houseArea'] : undefined,
+      vol_storage: vol_storage,
+      tank_cost: this.math.calc_cost_tank(this.estiForm, vol_storage),
       final_save: this.estiForm.value['final_save'] ? this.estiForm.value['final_save'] : undefined,
       rentability: undefined,
       created_at: this.estiForm.value['created_at'] ? this.estiForm.value['created_at'] : new Date(),
@@ -152,7 +124,7 @@ export class EstimationComponent implements OnInit, AfterViewInit {
       this.computeFirstDevis();
     }
     if ((this.estiForm.value['use'] == 'garden' && $event.selectedIndex == 6) ||
-    ((this.estiForm.value['use'] == 'both' || this.estiForm.value['use'] == 'house') && $event.selectedIndex == 7)) {
+      ((this.estiForm.value['use'] == 'both' || this.estiForm.value['use'] == 'house') && $event.selectedIndex == 7)) {
       this.computeFinalDevis();
     }
     console.log("test")
@@ -184,7 +156,7 @@ export class EstimationComponent implements OnInit, AfterViewInit {
   }
 
   checkIfTank() {
-    return (this.estiForm.value['tank_dist'] === null || this.water_volume<1000);
+    return (this.estiForm.value['tank_dist'] === null || this.water_volume < 1000);
   }
 
   goFurther() {
--- En-Auto-Frontend/src/app/service/math.service.ts
+++ En-Auto-Frontend/src/app/service/math.service.ts
@@ -1,5 +1,4 @@
-
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Devis} from "../Interface/Interface.module";
 import {FormGroup} from "@angular/forms";
 
@@ -9,16 +8,18 @@ import {FormGroup} from "@angular/forms";
 
 export class MathService {
 
-  constructor() {}
+  constructor() {
+  }
+
 //Volume d’eau récupéré (en L/an)= Précipitation (en L/m²/an)* Surface de la toiture(m2) * Coefficient de perte (en fonction du type de toiture)
   //conversion en L
-  roofWaterVolume(area: number,rain: number, roof: number): number{
+  roofWaterVolume(area: number, rain: number, roof: number): number {
     //corrugated roof
     if (roof == 2) {
       return (area * rain * 0.8) //en L par an
     }
     //flat roof
-    if(roof == 3) {
+    if (roof == 3) {
       return (area * rain * 0.6) //en L par an
     } else { //tiles roof
       return (area * rain * 0.9) //en L par an
@@ -26,15 +27,16 @@ export class MathService {
   }
 
 //cout tuile
-  tilesReparationCost(tiles:number) : number{
+  tilesReparationCost(tiles: number): number {
     return tiles * 2;
   }
 
-  gutterReparationCost(gutter:number) : number{
+  gutterReparationCost(gutter: number): number {
     return gutter * 15;
   }
+
 //calcule la consommation du foyer
-  calc_conso(estiForm: FormGroup) : number {
+  calc_conso(estiForm: FormGroup): number {
     //wc+Nettoyage+machines
     let conso: number;
     let people = estiForm.value["people"];
@@ -42,56 +44,54 @@ export class MathService {
     let garden_area = estiForm.value["garden_area"];
 
     conso = 8500 * people + 1100 * people;
-    console.log("machine: "+nb_machin);
+    console.log("machine: " + nb_machin);
     if (nb_machin > 0) {
       conso = conso + (nb_machin * 3500);
       console.log("conso: " + conso);
     }
-    if(garden_area>0) {
+    if (garden_area > 0) {
       conso = conso + (garden_area * 70);
       console.log("conso: " + conso);
     }
     return conso;
   }
 
-  calc_vol_storage(water_volume: number, conso : number) : number {
+  calc_vol_storage(water_volume: number, conso: number): number {
     let moy: number;
-    moy = (conso + water_volume ) / 2;
-    var resultat = (moy * 21)/365
-    var around =  Math.round(resultat);
+    moy = (conso + water_volume) / 2;
+    var resultat = (moy * 21) / 365
+    var around = Math.round(resultat);
     return around;
   }
 
-  calc_cost_tank(estiForm: FormGroup, vol : number) : number {
+  calc_cost_tank(estiForm: FormGroup, vol: number): number {
     let cost: number;
 
-    if(estiForm.value["tank_type"].match("dig")){
-      if(vol <= 1500) {
+    if (estiForm.value["tank_type"].match("dig")) {
+      if (vol <= 1500) {
         cost = 1900;
       }
-      if( (vol > 1500) && (vol <= 3000) ) {
+      if ((vol > 1500) && (vol <= 3000)) {
         cost = 2600;
       }
-      if ( (vol > 3000) && (vol <= 5000)) {
+      if ((vol > 3000) && (vol <= 5000)) {
         cost = 3000;
       }
-      if (vol>5000) {
+      if (vol > 5000) {
         cost = 3800;
       }
-    }
-    else if (estiForm.value["tank_type"].match("not-dig")) {
-      if(vol<=400){
+    } else if (estiForm.value["tank_type"].match("not-dig")) {
+      if (vol <= 400) {
         cost = 200;
-      }
-      else
+      } else
         cost = 500;
     }
     cost = cost + estiForm.value["tank_dist"] * 50;
     return (cost);
   }
 
-  totalCost(devis: Devis) : number {
-     return (devis.routing_cost.valueOf() + devis.structural_cost.valueOf() + devis.tank_cost.valueOf() + devis.water_cost.valueOf()*devis.consum.valueOf());
+  totalCost(devis: Devis): number {
+    return (devis.routing_cost.valueOf() + devis.structural_cost.valueOf() + devis.tank_cost.valueOf() + devis.water_cost.valueOf() * devis.consum.valueOf());
   }
 
 }
