a710095d98de2e293c7f34d4741194dcdb7e7e83
reilbat <reilbat25@gmail.com>
2020-04-08 19:52:39 +0200

    Update Devis.component.ts

--- En-Auto-Frontend/src/app/profile/Devis.component.ts
+++ En-Auto-Frontend/src/app/profile/Devis.component.ts
@@ -67,36 +67,36 @@ export class DevisComponent implements OnInit {
 
   updateColor(): void {
     if (this.value > 100) {
-      this.color = '#2d9026'
+      this.color = '#07db09'
     }
-    if (this.value <= 100 && this.value > 90) {
+    else if (this.value <= 100 && this.value > 90) {
       this.color = '#4b9026'
     }
-    if (this.value <= 90 && this.value > 80) {
+    else if (this.value <= 90 && this.value > 80) {
       this.color = '#86a531'
     }
-    if (this.value <= 80 && this.value > 70) {
+    else if (this.value <= 80 && this.value > 70) {
       this.color = '#cebd3b'
     }
-    if (this.value <= 70 && this.value > 60) {
+    else if (this.value <= 70 && this.value > 60) {
       this.color = '#b1aa29'
     }
-    if (this.value >= 60 && this.value > 50) {
+    else if (this.value >= 60 && this.value > 50) {
       this.color = '#a0912c'
     }
-    if (this.value <= 50 && this.value > 40) {
+    else if (this.value <= 50 && this.value > 40) {
       this.color = '#ba6012'
     }
-    if (this.value <= 40 && this.value > 30) {
+    else if (this.value <= 40 && this.value > 30) {
       this.color = '#a33e0b'
     }
-    if (this.value <= 30 && this.value > 20) {
+    else if (this.value <= 30 && this.value > 20) {
       this.color = '#ac4330'
     }
-    if (this.value <= 20 && this.value > 10) {
+    else if (this.value <= 20 && this.value > 10) {
       this.color = '#923b2c'
     }
-    if (this.value <= 10) {
+    else if (this.value <= 10) {
       this.color = '#9e2828'
     }
 
